6fe9d62e14e4f980d1947681c1b156fa24726ebc
Gabriel Duarte <65737170+djgabriel93@users.noreply.github.com>
2026-02-18 13:12:45 -0300

    fim do tratamento e inicio da analise

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -6,7 +6,17 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
-DADOS_ORIGINAIS = PASTA_DADOS / "exemplo.csv"
+orders = PASTA_DADOS / "olist_orders_dataset.csv"
+customers = PASTA_DADOS / "olist_customers_dataset.csv"
+items = PASTA_DADOS / "olist_order_items_dataset.csv"
+reviews = PASTA_DADOS / "olist_order_reviews_dataset.csv"
+products = PASTA_DADOS / "olist_products_dataset.csv"
+sellers = PASTA_DADOS / "olist_sellers_dataset.csv"
+payments = PASTA_DADOS / "olist_order_payments_dataset.csv"
+geolocation = PASTA_DADOS / "olist_geolocation_dataset.csv"
+
+DADOS_TRATADO = PASTA_DADOS / "olist_tratado.parquet"
+
 
 # coloque abaixo o caminho para os arquivos de modelos de seu projeto
 PASTA_MODELOS = PASTA_PROJETO / "modelos"
